4f411980d6c54326d8d6327a6945d03b446d0aef
Red-Portal <msca8h@naver.com>
2017-08-30 21:57:35 +0900

    added derived activation functions

--- include/tinynn/activation.hpp
+++ include/tinynn/activation.hpp
@@ -2,6 +2,7 @@
 #define _ACTIVATION_HPP_
 
 #include <cmath>
+#include <limits>
 
 namespace tnn::activation_function
 {
@@ -16,6 +17,25 @@ namespace tnn::activation_function
 
     inline double rectified_linear(double x) noexcept
     { return x < 0 ? 0 : x; }
+
+    template<double (*T)(double)>
+    double derived(double x) noexcept;
+
+    template<>
+    double derived<sigmoid>(double x) noexcept
+    { return sigmoid(x) * (1 - sigmoid(x)); }
+
+    template<>
+    double derived<identity>(double x) noexcept
+    { (void)x;  return 1; }
+
+    template<>
+    double derived<binary_step>(double x) noexcept
+    { return x == 0 ? std::numeric_limits<int32_t>::max() : 0; }
+
+    template<>
+    double derived<rectified_linear>(double x) noexcept
+    { return x < 0 ? 0 : 1; }
 }
 
 #endif
